c1a04633d8c35cad84efe026d76004b99dd57e62
Molly Soja <sojam@bc.edu>
2019-09-10 22:10:54 -0400

    5.5 complete

--- To Do List/To Do List/DetailViewController.swift
+++ To Do List/To Do List/DetailViewController.swift
@@ -11,15 +11,18 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var toDoField: UITextField!
+    
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
     var toDoItem: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        toDoField.becomeFirstResponder()
         if let toDoItem = toDoItem{
             toDoField.text = toDoItem
         }
-
+        enableDisableSaveButton()
+        toDoField.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
     
@@ -27,6 +30,18 @@ class DetailViewController: UIViewController {
         if segue.identifier == "UnwindFromSave"{
             toDoItem = toDoField.text
         }
+    }
+    
+    func enableDisableSaveButton() {
+        if let toDoFieldCount = toDoField.text?.count, toDoFieldCount > 0{
+            saveBarButton.isEnabled = true
+        } else {
+            saveBarButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func toDoFieldChanged(_ sender: UITextField) {
+        enableDisableSaveButton()
     }
     
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
