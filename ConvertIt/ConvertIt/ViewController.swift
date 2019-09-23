4aa3f73e4e5cd7939b232620edaf51498857edd7
Molly Soja <sojam@bc.edu>
2019-09-23 16:06:36 -0400

    6.5 complete

--- ConvertIt/ConvertIt/ViewController.swift
+++ ConvertIt/ConvertIt/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var formulaPicker: UIPickerView!
     
+    @IBOutlet weak var decimalSegment: UISegmentedControl!
     
     var formulaArray = ["miles to kilometers",
                         "kilometers to miles",
@@ -34,14 +35,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         formulaPicker.delegate = self
         formulaPicker.dataSource = self
+        conversionString = formulaArray[formulaPicker.selectedRow(inComponent: 0)]
     }
 
     func calculateConversion(){
         
-        var outputValue = 0.0
         
-        if let inputValue = Double(userInput.text!){
-         
+        
+        guard let inputValue = Double(userInput.text!) else {
+            print("Show alert here")
+            return
+        }
+            var outputValue = 0.0
             switch conversionString {
             case "miles to kilometers":
                 outputValue = inputValue / 0.62137
@@ -58,15 +63,20 @@ class ViewController: UIViewController {
             default:
                 print("show alert- no conversion")
             }
-            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits) "
-            
-        } else {
-            print("Show alert here")
+        let formatString = (decimalSegment.selectedSegmentIndex < decimalSegment.numberOfSegments-1 ? "%.\(decimalSegment.selectedSegmentIndex+1)f": "%f")
+        let outputString = String(format: formatString, outputValue )
+            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputString) \(toUnits) "
         }
+    
+    
+    @IBAction func decimalSelected(_ sender: UISegmentedControl) {
+        calculateConversion()
     }
     
     @IBAction func convertButtonPressed(_ sender: UIButton) {
+        calculateConversion()
     }
+
     
 }
 
