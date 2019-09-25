944c9271ddad5b6e1a14938523b4b1c7bc68aba9
Molly Soja <sojam@bc.edu>
2019-09-24 20:45:53 -0400

    7.3 complete

--- WeatherGift/WeatherGift/DetailVC.swift
+++ WeatherGift/WeatherGift/DetailVC.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class DetailVC: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        locationLabel.text = locationsArray[currentPage]
     }
 
 }
