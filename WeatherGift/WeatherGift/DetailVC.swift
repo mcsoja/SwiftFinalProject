a69b4a7274487bbcad397363889e2b2c37880e18
Molly Soja <sojam@bc.edu>
2019-10-18 16:03:46 -0400

    7.17 complete

--- WeatherGift/WeatherGift/DetailVC.swift
+++ WeatherGift/WeatherGift/DetailVC.swift
@@ -43,6 +43,8 @@ class DetailVC: UIViewController {
         locationLabel.text = locationsArray[currentPage].name
         dateLabel.text = locationsArray[currentPage].coordinates
         temperatureLabel.text = locationsArray[currentPage].currentTemp
+        summaryLabel.text = locationsArray[currentPage].currentSummary
+        currentImage.image = UIImage(named: locationsArray[currentPage].currentIcon)
     }
 
 }
--- WeatherGift/WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherGift/WeatherLocation.swift
@@ -14,6 +14,8 @@ class WeatherLocation {
     var name = ""
     var coordinates = ""
     var currentTemp = "--"
+    var currentSummary = ""
+    var currentIcon = ""
     
     func getWeather(completed: @escaping () -> ()){
         let weatherURL = urlBase + urlAPIKey + coordinates
@@ -26,6 +28,16 @@ class WeatherLocation {
                     self.currentTemp = roundedTemp + "°"
                 } else {
                     print("Could not return a temperature")
+                }
+                if let summary = json["daily"]["summary"].string{
+                    self.currentSummary = summary
+                } else {
+                    print("Could not return a summary")
+                }
+                if let icon = json["currently"]["icon"].string{
+                    self.currentIcon = icon
+                } else {
+                    print("Could not return an icon")
                 }
             case .failure(let error):
                 print(error)
