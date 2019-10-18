7222105a92d7007502975b19be4bc25c38ecc57b
Molly Soja <sojam@bc.edu>
2019-10-18 15:23:25 -0400

    7.16 complete

--- WeatherGift/WeatherGift/DetailVC.swift
+++ WeatherGift/WeatherGift/DetailVC.swift
@@ -24,7 +24,12 @@ class DetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUserInterface()
+        if currentPage != 0 {
+            self.locationsArray[currentPage].getWeather {
+                self.updateUserInterface()
+            }
+            
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -37,7 +42,7 @@ class DetailVC: UIViewController {
     func updateUserInterface() {
         locationLabel.text = locationsArray[currentPage].name
         dateLabel.text = locationsArray[currentPage].coordinates
-
+        temperatureLabel.text = locationsArray[currentPage].currentTemp
     }
 
 }
@@ -48,7 +53,6 @@ extension DetailVC: CLLocationManagerDelegate{
     func getLocation() {
         locationManager = CLLocationManager()
         locationManager.delegate = self
-        let status = CLLocationManager.authorizationStatus()
             
     }
     
@@ -89,8 +93,10 @@ extension DetailVC: CLLocationManagerDelegate{
             }
             self.locationsArray[0].name = place
             self.locationsArray[0].coordinates = currentCoordinates
-            self.locationsArray[0].getWeather()
-            self.updateUserInterface()
+            self.locationsArray[0].getWeather {
+                self.updateUserInterface()
+            }
+           
         })
     }
     
--- WeatherGift/WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherGift/WeatherLocation.swift
@@ -8,16 +8,29 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class WeatherLocation {
     var name = ""
     var coordinates = ""
+    var currentTemp = "--"
     
-    func getWeather(){
+    func getWeather(completed: @escaping () -> ()){
         let weatherURL = urlBase + urlAPIKey + coordinates
-        print(weatherURL)
         Alamofire.request(weatherURL).responseJSON { response in
-            print(response)
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                if let temperature = json["currently"]["temperature"].double{
+                    let roundedTemp = String(format: "%3.f", temperature)
+                    self.currentTemp = roundedTemp + "°"
+                } else {
+                    print("Could not return a temperature")
+                }
+            case .failure(let error):
+                print(error)
+            }
+        completed()
         }
     }
 }
