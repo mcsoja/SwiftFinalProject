25d9004727cc54444fdea0d5ae17a2091bc87394
Molly Soja <sojam@bc.edu>
2019-11-02 22:07:00 -0400

    9.5 - 9.7 complete

--- Snacktacular-Starter-Project-master/Snacktacular/Spot.swift
+++ Snacktacular-Starter-Project-master/Snacktacular/Spot.swift
@@ -9,8 +9,9 @@
 import Foundation
 import CoreLocation
 import Firebase
+import MapKit
 
-class Spot {
+class Spot: NSObject, MKAnnotation {
     var name: String
     var address: String
     var coordinate: CLLocationCoordinate2D
@@ -27,8 +28,16 @@ class Spot {
         return coordinate.latitude
     }
     
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        return address
+    }
+    
     var dictionary: [String: Any] {
-        return ["name": name, "address": address, "longitude": longitude, "latitude": latitude, "averageRating": averageRating, "numberOfReviews":numberOfReviews, "postingUserID": postingUserID]
+        return ["name": name, "address": address, "longitude": longitude, "latitude": latitude, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
     }
     
     init(name: String, address: String, coordinate: CLLocationCoordinate2D, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
@@ -42,8 +51,20 @@ class Spot {
         
     }
     
-    convenience init() {
+    override convenience init() {
         self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
+    }
+    
+    convenience init(dictionary: [String: Any]) {
+        let name = dictionary["name"] as! String? ?? ""
+        let address = dictionary["address"] as! String? ?? ""
+        let latitude = dictionary["latitude"] as! CLLocationDegrees? ?? 0.0
+        let longitude = dictionary["longitude"] as! CLLocationDegrees? ?? 0.0
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let averageRating = dictionary["averageRating"] as! Double? ?? 0.0
+        let numberOfReviews = dictionary["numberOfReviews"] as! Int? ?? 0
+        let postingUserID = dictionary["positngUserID"] as! String? ?? ""
+        self.init(name: name, address: address, coordinate: coordinate, averageRating: averageRating, numberOfReviews: numberOfReviews, postingUserID: postingUserID, documentID: "")
     }
     
     func saveData(completed: @escaping (Bool) -> ()) {
@@ -57,7 +78,6 @@ class Spot {
             let ref = db.collection("spots").document(self.documentID)
             ref.setData(dataToSave) { (error) in
                 if let error = error {
-                    
                     completed(false)
                 } else {
                     completed(true)
--- Snacktacular-Starter-Project-master/Snacktacular/Spots.swift
+++ Snacktacular-Starter-Project-master/Snacktacular/Spots.swift
@@ -7,7 +7,31 @@
 //
 
 import Foundation
+import Firebase
 
 class Spots {
     var spotArray = [Spot]()
+    
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()) {
+        db.collection("spots").addSnapshotListener { (querySnapshot, error) in
+            guard error == nil else {
+                print("*** error snapshot")
+                return completed()
+            }
+            self.spotArray = []
+            for document in querySnapshot!.documents {
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotArray.append(spot)
+            }
+            completed()
+        }
+    }
+    
 }
